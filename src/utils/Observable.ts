49378f9cecd0b7d946bb53ced932537e8bed89e6
MelvDouc <melv.douc@gmail.com>
2023-02-27 21:26:56 +0100

    Implement local Observable class

---
+++ src/utils/Observable.ts
@@ -0,0 +1,34 @@
+export default class Observable<T extends any> {
+  #value: T;
+  #subscriptions = new Set<(value: T) => any>();
+
+  constructor(value?: T) {
+    if (value !== undefined)
+      this.#value = value;
+  }
+
+  get value(): T {
+    return this.#value;
+  }
+
+  set value(value: T) {
+    this.#value = value;
+    this.notify();
+  }
+
+  subscribe(subscription: (value: T) => any): VoidFunction {
+    this.#subscriptions.add(subscription);
+    return () => this.#subscriptions.delete(subscription);
+  }
+
+  followObservable<O>(observable: Observable<O>, mapFn: (value: O) => T): this {
+    observable.subscribe((value) => {
+      this.value = mapFn(value);
+    });
+    return this;
+  }
+
+  notify(): void {
+    this.#subscriptions.forEach((subscription) => subscription(this.#value));
+  }
+}
