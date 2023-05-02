1cd00416461758070004890a7de0261dc0387487
MelvDouc <melv.douc@gmail.com>
2023-05-02 22:19:26 +0200

    Import Observable from separate package

--- src/types/Obs.ts
+++
@@ -1,8 +0,0 @@
-export interface Obs<T> {
-  value: T;
-  subscribe(subscription: (value: T) => void): VoidFunction;
-  map<U>(mapFn: (value: T) => U): Obs<U>;
-  notify(): void;
-}
-
-export type PossibleObs<T> = T | Obs<T>;
