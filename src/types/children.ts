d183f4e3d51277c6fbea75fc11c20ad63f0a1cfb
MelvDouc <melv.douc@gmail.com>
2023-05-02 17:31:40 +0200

    An observable comp child cannot be a Fragment

--- src/types/children.ts
+++ src/types/children.ts
@@ -2,6 +2,6 @@ import { Obs } from "./Obs.js";
 
 type PrimitiveChild = string | number | bigint | boolean | null | undefined;
 type PrimitiveOrNodeChild = PrimitiveChild | Node;
-export type ComponentChild = PrimitiveOrNodeChild | ComponentFactory | Obs<PrimitiveOrNodeChild>;
+export type ComponentChild = PrimitiveOrNodeChild | ComponentFactory | Obs<PrimitiveChild | Element>;
 export type ComponentChildren = ComponentChild | ComponentChild[];
 export type ComponentFactory<TProps extends { children?: ComponentChildren; } = any> = (props: TProps) => Node | ComponentFactory;
