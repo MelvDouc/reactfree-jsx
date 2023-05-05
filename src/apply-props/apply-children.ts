9a079819c2ec1c7d55ad0ad37a7e08087b09fed2
MelvDouc <melv.douc@gmail.com>
2023-05-05 16:40:04 +0200

    Throw error if child obs.value is a fragment

--- src/apply-props/apply-children.ts
+++ src/apply-props/apply-children.ts
@@ -5,20 +5,23 @@ export default function applyChildren(
   element: Pick<Element, "append" | "replaceChild">,
   children: ComponentChildren
 ): void {
-  if (children instanceof Node || typeof children !== "object" && !!children) {
+  if (children instanceof Node || typeof children !== "object" && !isFalseOrNullish(children)) {
     element.append(children as Node | string);
     return;
   }
 
   if (children instanceof Observable) {
+    if (children.value instanceof DocumentFragment)
+      throw new Error("An observable that is the child of an element cannot use a fragment as its value. Consider wrapping the child element in a div or span.");
+
     let prevNode: Node = (children.value instanceof Node)
       ? children.value
-      : document.createTextNode(children.value ? String(children.value) : "");
+      : document.createTextNode(!isFalseOrNullish(children.value) ? String(children.value) : "");
     element.append(prevNode);
     children.subscribe((value) => {
       const newNode = (value instanceof Node)
         ? value
-        : document.createTextNode(value ? String(value) : "");
+        : document.createTextNode(!isFalseOrNullish(value) ? String(value) : "");
       element.replaceChild(newNode, prevNode);
       prevNode = newNode;
     });
@@ -26,4 +29,8 @@ export default function applyChildren(
 
   if (Array.isArray(children))
     children.forEach((child) => applyChildren(element, child));
+}
+
+function isFalseOrNullish(value: unknown): value is false | null | undefined {
+  return value === false || false == null;
 }
