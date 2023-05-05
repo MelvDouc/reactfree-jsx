03c18d8376eb5030bb4d6fd570267ddc4d308db8
MelvDouc <melv.douc@gmail.com>
2023-05-05 16:29:21 +0200

    Fix support for boolean attributes

--- src/apply-props/apply-props.ts
+++ src/apply-props/apply-props.ts
@@ -1,12 +1,37 @@
 import { Observable, OptionalObs } from "melv_observable";
 
+const booleanAttributes = {
+  allowfullscreen: "allowfullscreen",
+  async: "async",
+  autofocus: "autofocus",
+  autoplay: "autoplay",
+  checked: "checked",
+  controls: "controls",
+  default: "default",
+  defer: "defer",
+  disabled: "disabled",
+  formNoValidate: "formnovalidate",
+  inert: "inert",
+  isMap: "ismap",
+  loop: "loop",
+  multiple: "multiple",
+  muted: "muted",
+  noModule: "nomodule",
+  noValidate: "novalidate",
+  open: "open",
+  playsInline: "playsinline",
+  readOnly: "readonly",
+  required: "required",
+  reversed: "reversed",
+  selected: "selected"
+} as const;
+
 export default function applyProps<T extends keyof HTMLElementTagNameMap>(
   element: HTMLElementTagNameMap[T],
   props: Record<string, OptionalObs<any>>
 ): void {
   for (const key in props) {
-    // @ts-ignore
-    const item = props[key];
+    const item: OptionalObs<any> = props[key];
 
     if (!(item instanceof Observable)) {
       applyProp(element, key, item);
@@ -16,15 +41,15 @@ export default function applyProps<T extends keyof HTMLElementTagNameMap>(
     applyProp(element, key, item.value);
     item.subscribe((value) => applyProp(element, key, value));
 
-    if (key === "open") {
+    if (key in booleanAttributes) {
       new MutationObserver((mutations) => {
         for (const { attributeName } of mutations) {
-          if (attributeName === "open") {
-            item.value = element.getAttribute("open") !== null;
+          if (attributeName === booleanAttributes[key as keyof typeof booleanAttributes]) {
+            item.value = element.hasAttribute(attributeName);
             return;
           }
         }
-      }).observe(element, { attributeFilter: ["open"] });
+      }).observe(element, { attributes: true });
     }
   }
 }
